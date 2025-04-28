7987336c4aac036020eae8369a6fc7ce593e54d4
hkereb <115366640+hkereb@users.noreply.github.com>
2025-04-28 19:32:58 +0200

    Initial commit

---
+++ src/main/java/com/github/hkereb/swiftcodeapi/SwiftCodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.hkereb.swiftcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCodeApiApplication.class, args);
+	}
+
+}
